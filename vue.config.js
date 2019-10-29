eb07e965ed5a75d488134d57e3cacd01f2bda732
tschuhmacher <t.schuhmacher+smb@gmail.com>
2019-10-29 19:30:03 +0100

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/pwa/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   outputDir: "pwa",
   pwa: {
     name: "Bronzen wie Tiere",
