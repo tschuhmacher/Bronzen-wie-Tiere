be98a9dab2d5e80ab5a53b44975858b33125af34
tschuhmacher <t.schuhmacher+smb@gmail.com>
2019-11-15 15:29:36 +0100

    prettify

--- src/components/model-viewer/model-mixin.vue
+++ src/components/model-viewer/model-mixin.vue
@@ -1,14 +1,15 @@
 <template>
   <div style="width: 100%; height: 100%; margin: 0; border: 0; padding: 0;">
-    <canvas v-if="suportWebGL" ref="canvas" style="width: 100%; height: 100%;"></canvas>
+    <canvas v-if="suportWebGL" ref="canvas" style="width: 100%; height: 100%;">
+    </canvas>
     <div v-else>
       <slot>
         Your browser does not seem to support
         <a
           href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation"
           style="color:#000"
-        >WebGL</a>.
-        <br>'
+          >WebGL</a
+        >. <br />'
       </slot>
     </div>
   </div>
