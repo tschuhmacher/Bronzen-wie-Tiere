aa1b9c675e120b414cdd022321d452fe436b03e5
tschuhmacher <t.schuhmacher+smb@gmail.com>
2020-10-21 15:23:19 +0200

    production folder = "/"

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "mk-pwa" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   outputDir: "mk-pwa",
   pwa: {
     name: "Bronzen wie Tiere",
